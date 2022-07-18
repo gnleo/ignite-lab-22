ba9493bc4a920b0588bd624a0a9f8732b5a93d85
Leonardo Nunes Goncalves <jamill_gnleo@yahoo.com>
2022-07-17 23:57:03 -0300

    aula 2

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,28 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+// module.exports = getDefaultConfig(__dirname);
+
+// ------------------------------
+// ACIMA FOI SUBSTITUÍDO POR CAUSA DA LIB REACT-SVG-TRANSFORMER
+
+
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
